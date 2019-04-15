1f37bff20690865cc09503f215f9b375b0d911d2
synzen <michael994664@gmail.com>
2019-04-14 22:46:47 -0400

    Check for null exceptions and add description

--- UnburnableMeat.cs
+++ UnburnableMeat.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 namespace Oxide.Plugins
 {
     [Info("Unburnable Meat", "S642667", "1.0.0")]
+    [Description("Prevent cooked meats from burning")]
     class UnburnableMeat : RustPlugin
     {
         public static readonly string[] CookedItems = new string[]
@@ -20,11 +21,32 @@ namespace Oxide.Plugins
         private Dictionary<string, int> lowTemps = new Dictionary<string, int>();
         private Dictionary<string, int> highTemps = new Dictionary<string, int>();
 
+        ItemModCookable getCookable (string shortname)
+        {
+            var definition = ItemManager.FindItemDefinition(shortname);
+            if (definition == null)
+            {
+                Puts($"Uknown definition for {shortname}");
+                return null;
+            }
+            var cookable = definition.GetComponent<ItemModCookable>();
+            if (cookable == null)
+            {
+                Puts($"Uknown cookable for {shortname}");
+                return null;
+            }
+            return cookable;
+        }
+
         void OnServerInitialized()
         {
             foreach (var shortname in CookedItems)
             {
-                var cookable = ItemManager.FindItemDefinition(shortname).GetComponent<ItemModCookable>();
+                var cookable = getCookable(shortname);
+                if (cookable == null)
+                {
+                    continue;
+                }
                 lowTemps.Add(shortname, cookable.lowTemp);
                 highTemps.Add(shortname, cookable.highTemp);
                 cookable.lowTemp = -1;
@@ -34,11 +56,15 @@ namespace Oxide.Plugins
 
         void Unload()
         {
-            foreach (var shortname in CookedItems)
+            foreach (KeyValuePair<string, int> item in lowTemps)
             {
-                var cookable = ItemManager.FindItemDefinition(shortname).GetComponent<ItemModCookable>();
-                cookable.lowTemp = lowTemps[shortname];
-                cookable.highTemp = highTemps[shortname];
+                var cookable = getCookable(item.Key);
+                if (cookable == null)
+                {
+                    continue;
+                }
+                cookable.lowTemp = item.Value;
+                cookable.highTemp = highTemps[item.Key];
             }
         }
     }
